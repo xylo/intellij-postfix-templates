b1bff3e9765852e3443cf412a559fdb12f7e67a8
Stefan Endrullis <stefan@endrullis.de>
2017-08-09 18:40:32 +0200

    added tool to test the templates

---
+++ test/src/de/endrullis/idea/postfixtemplates/templates/TemplateExtractionTestApp.java
@@ -0,0 +1,88 @@
+package de.endrullis.idea.postfixtemplates.templates;
+
+import java.io.*;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static de.endrullis.idea.postfixtemplates.utils.CollectionUtils._List;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * @author Stefan Endrullis &lt;stefan@endrullis.de&gt;
+ */
+public class TemplateExtractionTestApp {
+
+	public static void main(String[] args) throws IOException {
+		File javaTemplateFile = new File("test/src/de/endrullis/idea/postfixtemplates/templates/TemplatesTestTemplate.java");
+		File javaOutputFile   = new File("test/src/de/endrullis/idea/postfixtemplates/templates/TemplatesTestOutput.java");
+
+		List<String> javaTemplateLines = Files.readAllLines(javaTemplateFile.toPath(), Charset.forName("UTF-8"));
+
+		try (PrintStream out = new PrintStream(javaOutputFile)) {
+			for (String javaLine : javaTemplateLines) {
+				if (javaLine.contains("// PLACEHOLDER")) {
+					printTemplates(out);
+				} else {
+					out.println(replace(javaLine, "TemplatesTestTemplate", "TemplatesTestOutput"));
+				}
+			}
+		}
+	}
+
+	private static String replace(String input, String from, String to) {
+		return input.replaceAll(Pattern.quote(from), Matcher.quoteReplacement(to));
+	}
+
+	private static void printTemplates(PrintStream out) {
+		InputStream templatesIn = TemplateExtractionTestApp.class.getResourceAsStream("/de/endrullis/idea/postfixtemplates/language/defaulttemplates/java.postfixTemplates");
+		Stream<String> templateLines = new BufferedReader(new InputStreamReader(templatesIn)).lines().filter(s -> s.contains("→"));
+
+		InputStream typeMappingIn = TemplateExtractionTestApp.class.getResourceAsStream("/de/endrullis/idea/postfixtemplates/templates/typeMapping.txt");
+		Map<String, String> type2var = new BufferedReader(new InputStreamReader(typeMappingIn)).lines().filter(s -> s.contains("→"))
+			.map(l -> l.split("→")).collect(Collectors.toMap(a -> a[0].trim(), a -> a[1].trim()));
+
+		templateLines.forEach(line -> {
+			String[] parts = line.split("→");
+
+			assertTrue(parts[1].contains("$expr$"));
+
+			String expr = type2var.get(parts[0].trim());
+			if (expr == null) {
+				throw new RuntimeException("No type mapping found for " + parts[0]);
+			}
+
+			String s = replace(parts[1], "$expr$", expr);
+			for (String from : _List("$keyF$", "$valueF$", "$attributeF$", "$f$")) {
+				s = replace(s, from, "x -> x");
+			}
+			s = replace(s, "$key$", "\"key\"");
+			s = replace(s, "$condition$", "x -> true");
+			s = replace(s, "$stream$", "stream");
+			s = replace(s, "$endAction$", "x -> {}");
+			s = replace(s, "$action$", "x -> {}");
+			s = replace(s, "$separator$", "\", \"");
+			s = replace(s, "$accumulator$", "(a, b) -> b");
+			s = replace(s, "$neutralElement$", "\"\"");
+			s = replace(s, "$i$", "0");
+			s = replace(s, "$encoding::\"\\\"UTF-8\\\"\"$", "\"UTF-8\"");
+			s = replace(s, "$var:suggestVariableName()$", "x");
+
+			s = replace(s, "$object$", "object");
+			s = replace(s, "$doubleValue$", "0d");
+			s = replace(s, "$intValue$", "0");
+			s = replace(s, "$longValue$", "0L");
+
+
+			String res = "f(() -> " + s + ");";
+
+			out.println(res);
+		});
+	}
+
+}
---
+++ test/src/de/endrullis/idea/postfixtemplates/templates/TemplatesTestTemplate.java
@@ -0,0 +1,74 @@
+package de.endrullis.idea.postfixtemplates.templates;
+
+import org.apache.commons.lang.ArrayUtils;
+
+import java.io.*;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.*;
+import java.util.function.*;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
+
+/**
+ * @author Stefan Endrullis &lt;stefan@endrullis.de&gt;
+ */
+public class TemplatesTestTemplate {
+
+	public static void main(String[] args) throws FileNotFoundException, MalformedURLException {
+		String[] array = new String[0];
+		Set<String> set = new HashSet<>();
+		List<String> list = new ArrayList<>();
+		Map<String,String> map = new HashMap<>();
+		Collection<String> collection = list;
+		Iterable<String> iterable = list;
+		Optional<String> optional = Optional.empty();
+		Stream<String> stream = list.stream();
+		String string = "";
+		Integer number = 1;
+		int n = 1;
+		Object object = null;
+		File file = new File(".");
+	  Path path = file.toPath();
+		InputStream in = new FileInputStream(file);
+		URL url = new URL("");
+		Runnable runnable = () -> {};
+		Supplier             supplier = null;
+		BooleanSupplier      booleanSupplier = null;
+		DoubleSupplier       doubleSupplier = null;
+		IntSupplier          intSupplier = null;
+		LongSupplier         longSupplier = null;
+		Consumer             consumer = null;
+		BiConsumer           biConsumer = null;
+		DoubleConsumer       doubleConsumer = null;
+		IntConsumer          intConsumer = null;
+		LongConsumer         longConsumer = null;
+		ObjDoubleConsumer    objDoubleConsumer = null;
+		ObjIntConsumer       objIntConsumer = null;
+		ObjLongConsumer      objLongConsumer = null;
+		Predicate            predicate = null;
+		BiPredicate          biPredicate = null;
+		DoublePredicate      doublePredicate = null;
+		IntPredicate         intPredicate = null;
+		LongPredicate        longPredicate = null;
+		
+	  // PLACEHOLDER
+	}
+
+	private static void f(ExceptionRunnable runnable) {}
+	private static void f(ExceptionSupplier runnable) {}
+
+	public interface ExceptionRunnable {
+		void run() throws Exception;
+	}
+
+	public interface ExceptionSupplier {
+		Object run() throws Exception;
+	}
+
+}
