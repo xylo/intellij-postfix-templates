45fa9d6ae343e5c46dea15eaf311b06910fdea1d
Stefan Endrullis <stefan@endrullis.de>
2018-01-20 14:31:00 +0100

    restructured tools menu

---
+++ src/de/endrullis/idea/postfixtemplates/actions/OpenSettingsAction.java
@@ -0,0 +1,23 @@
+package de.endrullis.idea.postfixtemplates.actions;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.options.ShowSettingsUtil;
+import com.intellij.openapi.project.Project;
+import de.endrullis.idea.postfixtemplates.settings.CptPluginConfigurable;
+
+/**
+ * Action to open the templates of the language in the current editor.
+ *
+ * @author Stefan Endrullis &lt;stefan@endrullis.de&gt;
+ */
+public class OpenSettingsAction extends AnAction {
+	@Override
+	public void actionPerformed(AnActionEvent anActionEvent) {
+		Project project = anActionEvent.getProject();
+
+		if (project != null) {
+			ShowSettingsUtil.getInstance().showSettingsDialog(project, CptPluginConfigurable.class);
+		}
+	}
+}
