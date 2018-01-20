0785f0dede96655179a818752edd1a6bfadd1aa7
Stefan Endrullis <stefan@endrullis.de>
2018-01-20 12:37:21 +0100

    forgot to commit

--- src/de/endrullis/idea/postfixtemplates/settings/CptPluginSettingsForm.java
+++ src/de/endrullis/idea/postfixtemplates/settings/CptPluginSettingsForm.java
@@ -6,7 +6,6 @@ import com.intellij.diff.contents.DocumentContent;
 import com.intellij.diff.requests.SimpleDiffRequest;
 import com.intellij.openapi.Disposable;
 import com.intellij.openapi.application.ApplicationManager;
-import com.intellij.openapi.application.ReadAction;
 import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.editor.EditorFactory;
@@ -16,8 +15,10 @@ import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.PsiDocumentManager;
 import com.intellij.psi.PsiManager;
 import de.endrullis.idea.postfixtemplates.language.CptFileType;
+import de.endrullis.idea.postfixtemplates.language.CptLang;
 import de.endrullis.idea.postfixtemplates.language.CptUtil;
 import de.endrullis.idea.postfixtemplates.language.psi.CptFile;
+import de.endrullis.idea.postfixtemplates.languages.SupportedLanguages;
 import de.endrullis.idea.postfixtemplates.templates.CustomPostfixTemplateProvider;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -31,16 +32,15 @@ import java.io.InputStreamReader;
 import static de.endrullis.idea.postfixtemplates.utils.StringUtils.replace;
 
 public class CptPluginSettingsForm implements CptPluginSettings.Holder, Disposable {
-	private JPanel       mainPanel;
-	private JPanel       templatesEditorPanel;
-	private JButton      editButton;
-	private JRadioButton emptyLambdaRadioButton;
-	private JRadioButton varLambdaRadioButton;
-	private JButton      resetYourTemplatesButton;
-	private JButton      showDiffButton;
+	private JPanel         mainPanel;
+	private JPanel         templatesEditorPanel;
+	private JButton        editButton;
+	private JRadioButton   emptyLambdaRadioButton;
+	private JRadioButton   varLambdaRadioButton;
+	private JButton        resetYourTemplatesButton;
+	private JButton        showDiffButton;
+	private JList<CptLang> languageList;
 
-	@NotNull
-	private String templatesText = "";
 	@Nullable
 	private Editor templatesEditor;
 
@@ -49,6 +49,14 @@ public class CptPluginSettingsForm implements CptPluginSettings.Holder, Disposab
 	}
 
 	public JComponent getComponent() {
+		languageList.setModel(new DefaultListModel<CptLang>() {{
+			SupportedLanguages.supportedLanguages.forEach(l -> addElement(l));
+		}});
+		languageList.getSelectionModel().addListSelectionListener(e -> {
+			updateEditorContent();
+		});
+		languageList.setSelectedValue(SupportedLanguages.getCptLang("java"), true);
+
 		editButton.addActionListener(e -> openTemplatesInEditor());
 		resetYourTemplatesButton.addActionListener(e -> resetTemplates());
 		showDiffButton.addActionListener(e -> showDiff());
@@ -71,7 +79,15 @@ public class CptPluginSettingsForm implements CptPluginSettings.Holder, Disposab
 			emptyLambdaRadioButton.setSelected(true);
 		}
 
-		final String[] templatesText = {CptUtil.getDefaultTemplates("java")};
+		updateEditorContent(preFilled);
+	}
+
+	private void updateEditorContent() {
+		updateEditorContent(varLambdaRadioButton.isSelected());
+	}
+
+	private void updateEditorContent(boolean preFilled) {
+		final String[] templatesText = {CptUtil.getDefaultTemplates(getSelectedLang().getLanguage())};
 
 		new BufferedReader(new InputStreamReader(
 			CptUtil.class.getResourceAsStream("templatemapping/" + (preFilled ? "var" : "empty") + "Lambda.txt")
@@ -93,7 +109,7 @@ public class CptPluginSettingsForm implements CptPluginSettings.Holder, Disposab
 	}
 
 	private void openTemplatesInEditor() {
-		File file = CptUtil.getTemplateFile("java").get();
+		File file = CptUtil.getTemplateFile(getSelectedLang().getLanguage()).get();
 
 		Project project = CptUtil.getActiveProject();
 
@@ -104,13 +120,13 @@ public class CptPluginSettingsForm implements CptPluginSettings.Holder, Disposab
 	}
 
 	private void resetTemplates() {
-		CptUtil.createTemplateFile("java", templatesText);
+		CptUtil.createTemplateFile(getSelectedLang().getLanguage(), getTemplateText());
 
 		//ApplicationManager.getApplication().invokeLater(() -> {
 		Project project = CptUtil.getActiveProject();
 
 		// refresh file content
-		File file = CptUtil.getTemplateFile("java").get();
+		File file = CptUtil.getTemplateFile(getSelectedLang().getLanguage()).get();
 		VirtualFile vFile = LocalFileSystem.getInstance().refreshAndFindFileByIoFile(file);
 		assert vFile != null;
 		vFile.refresh(false, false);
@@ -125,7 +141,6 @@ public class CptPluginSettingsForm implements CptPluginSettings.Holder, Disposab
 
 		// tell template provider to reload the templates
 		ApplicationManager.getApplication().getMessageBus().syncPublisher(CustomPostfixTemplateProvider.TOPIC).reloadTemplates();
-		//});
 	}
 
 	private void closeSettings() {
@@ -136,10 +151,10 @@ public class CptPluginSettingsForm implements CptPluginSettings.Holder, Disposab
 	private void showDiff() {
 		Project project = CptUtil.getActiveProject();
 
-		CptUtil.getTemplateFile("java").ifPresent(file -> {
+		CptUtil.getTemplateFile(getSelectedLang().getLanguage()).ifPresent(file -> {
 			VirtualFile vFile = LocalFileSystem.getInstance().findFileByIoFile(file);
 
-			DocumentContent content1 = DiffContentFactory.getInstance().create(templatesText);
+			DocumentContent content1 = DiffContentFactory.getInstance().create(getTemplateText());
 			DocumentContent content2 = DiffContentFactory.getInstance().createDocument(project, vFile);
 			DiffManager.getInstance().showDiff(project, new SimpleDiffRequest("Templates Diff", content1, content2,
 				"Predefined plugin templates", "Your templates"));
@@ -173,9 +188,6 @@ public class CptPluginSettingsForm implements CptPluginSettings.Holder, Disposab
 	@NotNull
 	@Override
 	public CptPluginSettings getPluginSettings() {
-		if (templatesEditor != null && !templatesEditor.isDisposed()) {
-			templatesText = ReadAction.compute(() -> templatesEditor.getDocument().getText());
-		}
 		return new CptPluginSettings(varLambdaRadioButton.isSelected());
 	}
 
@@ -187,4 +199,11 @@ public class CptPluginSettingsForm implements CptPluginSettings.Holder, Disposab
 		templatesEditor = null;
 	}
 
+	public CptLang getSelectedLang() {
+		return languageList.getSelectedValue();
+	}
+
+	public String getTemplateText() {
+		return CptUtil.getDefaultTemplates(getSelectedLang().getLanguage());
+	}
 }
