c7b62abaad30f784fdb05b8079ea1d7c47fbb1f3
Stefan Endrullis <stefan@endrullis.de>
2018-04-03 20:13:55 +0200

    minor changes

--- src/de/endrullis/idea/postfixtemplates/languages/scala/ScalaPostfixTemplatesUtils.java
+++ src/de/endrullis/idea/postfixtemplates/languages/scala/ScalaPostfixTemplatesUtils.java
@@ -9,19 +9,19 @@ import java.util.stream.Stream;
 /**
  * @author Stefan Endrullis &lt;stefan@endrullis.de&gt;
  */
-public class ScalaPostfixTemplatesUtils {
+class ScalaPostfixTemplatesUtils {
 
-	public static final Condition<PsiElement> BYTE     = isDescendant("scala.Byte", "java.lang.Byte");
-	public static final Condition<PsiElement> CHAR     = isDescendant("scala.Char", "java.lang.Character");
-	public static final Condition<PsiElement> DOUBLE   = isDescendant("scala.Double", "java.lang.Double");
-	public static final Condition<PsiElement> FLOAT    = isDescendant("scala.Float", "java.lang.Float");
-	public static final Condition<PsiElement> INT      = isDescendant("scala.Int", "java.lang.Integer");
-	public static final Condition<PsiElement> LONG     = isDescendant("scala.Long", "java.lang.Long");
-	public static final Condition<PsiElement> SHORT    = isDescendant("scala.Short", "java.lang.Short");
-	public static final Condition<PsiElement> BOOLEAN  = isDescendant("scala.Boolean", "java.lang.Boolean");
-	public static final Condition<PsiElement> VOID     = isDescendant("scala.Unit", "java.lang.Void");
-	public static final Condition<PsiElement> NON_VOID = e -> !VOID.value(e);
-	public static final Condition<PsiElement> DECIMAL_NUMBER = e ->
+	static final Condition<PsiElement> BYTE     = isDescendant("scala.Byte", "java.lang.Byte");
+	static final Condition<PsiElement> CHAR     = isDescendant("scala.Char", "java.lang.Character");
+	static final Condition<PsiElement> DOUBLE   = isDescendant("scala.Double", "java.lang.Double");
+	static final Condition<PsiElement> FLOAT    = isDescendant("scala.Float", "java.lang.Float");
+	static final Condition<PsiElement> INT      = isDescendant("scala.Int", "java.lang.Integer");
+	static final Condition<PsiElement> LONG     = isDescendant("scala.Long", "java.lang.Long");
+	static final Condition<PsiElement> SHORT    = isDescendant("scala.Short", "java.lang.Short");
+	static final Condition<PsiElement> BOOLEAN  = isDescendant("scala.Boolean", "java.lang.Boolean");
+	static final Condition<PsiElement> VOID     = isDescendant("scala.Unit", "java.lang.Void");
+	static final Condition<PsiElement> NON_VOID = e -> !VOID.value(e);
+	static final Condition<PsiElement> DECIMAL_NUMBER = e ->
 		Stream.of(BYTE, CHAR, DOUBLE, FLOAT, INT, LONG, SHORT).anyMatch(t -> t.value(e));
 
 	private static Condition<PsiElement> isDescendant(String class1, String class2) {
