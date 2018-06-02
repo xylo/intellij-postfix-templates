9f7e0e8b32c45317f0a0b6280636c280985db746
Stefan Endrullis <stefan@endrullis.de>
2018-06-02 12:26:45 +0200

    replaced static file

--- src/de/endrullis/idea/postfixtemplates/settings/WebTemplateFileLoader.java
+++ src/de/endrullis/idea/postfixtemplates/settings/WebTemplateFileLoader.java
@@ -2,10 +2,7 @@ package de.endrullis.idea.postfixtemplates.settings;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
-import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
-import org.apache.commons.lang3.builder.ToStringStyle;
 
-import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 
@@ -18,7 +15,7 @@ public class WebTemplateFileLoader {
 
 	public static WebTemplateFile[] load(URL url) throws IOException {
 		ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
-		return mapper.readValue(new File("test/src/yaml/webTemplateFiles.yaml"), WebTemplateFile[].class);
+		return mapper.readValue(url, WebTemplateFile[].class);
 	}
 
 }
