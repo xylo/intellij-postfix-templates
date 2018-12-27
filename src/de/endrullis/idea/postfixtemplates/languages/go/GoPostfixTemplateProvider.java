44b3e09211e02d3183b22f59f8247105d294896d
Phil <philip.griggs@graphmasters.net>
2018-12-27 23:06:37 +0000

    getPluginClassName method

--- src/de/endrullis/idea/postfixtemplates/languages/go/GoPostfixTemplateProvider.java
+++ src/de/endrullis/idea/postfixtemplates/languages/go/GoPostfixTemplateProvider.java
@@ -7,16 +7,22 @@ import org.jetbrains.annotations.NotNull;
 
 public class GoPostfixTemplateProvider extends CustomPostfixTemplateProvider {
 
-	@NotNull
-	@Override
-	protected String getLanguage() {
-		return "go";
-	}
+    @NotNull
+    @Override
+    protected String getLanguage() {
+        return "go";
+    }
 
-	@NotNull
-	@Override
-	protected CustomGoStringPostfixTemplate createTemplate(CptMapping mapping, String matchingClass, String conditionClass, String templateName, String description, String template, PostfixTemplateProvider provider) {
-		return new CustomGoStringPostfixTemplate(matchingClass, templateName, description, template, provider, mapping);
-	}
+    @Override
+    public String getPluginClassName() {
+        return "com.goide.psi.GoExpression";
+    }
+
+
+    @NotNull
+    @Override
+    protected CustomGoStringPostfixTemplate createTemplate(CptMapping mapping, String matchingClass, String conditionClass, String templateName, String description, String template, PostfixTemplateProvider provider) {
+        return new CustomGoStringPostfixTemplate(matchingClass, templateName, description, template, provider, mapping);
+    }
 
 }
